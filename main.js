d9fdf6e8b0cf81764d415924ea046787708a5122
rahul253040 <mdrahulsarkar5525@gmail.com>
2024-07-26 09:50:47 +0600

    update

---
+++ main.js
@@ -0,0 +1,12 @@
+let popup = document.getElementById('popup');
+
+function openpopup() {
+   popup.classList.add('open-popup');
+}
+
+let okBtn = document.querySelector('.ok-btn');
+
+function closepopup() {
+    popup.classList.remove('open-popup');
+};
+
